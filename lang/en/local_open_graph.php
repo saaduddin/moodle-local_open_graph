84f7e51c88f767c7ab2794d67e802222eb9c5fc0
Saad <saad.uddin@outlook.com>
2025-05-11 21:44:13 +0000

    Add fixes of local_open_graph plugin with requested changes

--- lang/en/local_open_graph.php
+++ lang/en/local_open_graph.php
@@ -1,4 +1,25 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+//
+// @copyright 2025 Saad Uddin
+// @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+// @package   local_open_graph
+
 $string['pluginname'] = 'Open Graph';
 $string['defaultdescription'] = 'Default description';
 $string['defaultdescription_desc'] = 'This description will be used if no specific one is set.';
+$string['privacy:metadata'] = 'The Open Graph plugin does not store any personal data.';
+$string['cachedef_opengraphtags'] = 'Cache for Open Graph tags';
